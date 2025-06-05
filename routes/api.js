8623b56642c3e5955de6a8aee005f577031473da
MilenaRodriguezzz <milenaagustinarodriguezz@gmail.com>
2025-06-05 15:18:26 -0300

    Mile: Productos agregados a la base de datos y metodos POST y GET creados

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router(); // Importamos express y creamos un router
 
-// Ruta de prueba
-router.get('/', (req, res) => {
-  res.send('API funcionando');
+const Producto = require('../models/Producto');
+
+router.post('/productos', async (req, res) => {
+  try {
+    const nuevo = new Producto(req.body);
+    await nuevo.save();
+    res.status(201).json({ mensaje: ' ✅ Producto guardado con éxito' });
+  } catch (error) {
+    res.status(500).json({ error: ' ❌ Error al guardar producto' });
+  }
 });
+
+router.get('/productos', async (req, res) => {
+  try {
+    const productos = await Producto.find({ activo: true });
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ error: '❌ Error al obtener productos' });
+  }
+});
+
 
 module.exports = router;
